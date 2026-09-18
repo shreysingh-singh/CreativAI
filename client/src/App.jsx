def49dc5307123dce868c172be1f281435e0a78a
Shrey <shreysingh11733@gmail.com>
2026-09-18 14:47:46 +0530

    NavBar_Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,40 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home';
-import Layout from './pages/Layout';
-import Dashboard from './pages/Dashboard';
-import WriteAreticle from './pages/WriteAreticle';
-import GenerateImages from './pages/GenerateImages';
-import RemoveBackground from './pages/RemoveBackground';
-import RemoveObject from './pages/RemoveObject';
-import ReviewResume from './pages/ReviewResume';
-import Community from './pages/Community';
+import { Route, Routes } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import WriteAreticle from "./pages/WriteAreticle";
+import GenerateImages from "./pages/GenerateImages";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import ReviewResume from "./pages/ReviewResume";
+import Community from "./pages/Community";
+import BlogTittles from "./pages/blogTittles";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/ai' element={<Layout />}/>
-        <Route index element={<Dashboard />}>
-        <Route path='write-article' element={<WriteAreticle />}/>
-        <Route path='blog-tittles' element={<BlogTittles />} />
-        <Route path='generate-images' element={<GenerateImages />} />
-        <Route path='remove-background' element={<RemoveBackground />} />
-        <Route path='remove-object' element={<RemoveObject />} />
-        <Route path='review-resume' element={<ReviewResume />} />
-        <Route path='community' element={<Community />} />
+        {/* Home */}
+        <Route path="/" element={<Home />} />
+
+        {/* AI Dashboard */}
+        <Route path="/ai" element={<Layout />}>
+          {/* Default page: /ai */}
+          <Route index element={<Dashboard />} />
+
+          {/* AI Features */}
+          <Route path="write-article" element={<WriteAreticle />} />
+          <Route path="blog-tittles" element={<BlogTittles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="community" element={<Community />} />
         </Route>
-        
       </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/react";
+
+//Publish_key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if(!PUBLISHABLE_KEY){
+  throw new Error('Publish key is missing');
+}
 
 createRoot(document.getElementById("root")).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={'/'} >
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </ClerkProvider>,
 );
