f850fdc066edb705de1bcf3de91d62d9f8e1c64f
Shrey <shreysingh11733@gmail.com>
2026-09-25 16:46:05 +0530

    Article_page_08

--- client/src/pages/WriteAreticle.jsx
+++ client/src/pages/WriteAreticle.jsx
@@ -1,11 +1,114 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Edit, Sparkles } from "lucide-react";
 
 function WriteAreticle() {
+  const articleLength = [
+    { length: 800, text: "Short (500-800 words)" },
+    { length: 1200, text: "Medium (800-1200 words)" },
+    { length: 1600, text: "Long (+1200 words)" },
+  ];
+
+  const [selectedLength, setSelectedLength] = useState(articleLength[0]);
+  const [input, setInput] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      Hey this is an Article page 
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* Left Side */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200 shadow-md"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#4A7AFF]" />
+
+          <h1 className="text-xl font-semibold">AI Article Writer</h1>
+        </div>
+
+        <p className="mt-6 text-sm font-medium">Article Topic</p>
+
+        <input
+          type="text"
+          placeholder="The future of artificial intelligence"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+        />
+
+        <p className="mt-4 text-sm font-medium">Article Length</p>
+
+        <div className="mt-4 flex flex-wrap gap-3 sm:max-w-[90%]">
+          {articleLength.map((item, idx) => (
+            <span
+              key={idx}
+              onClick={() => setSelectedLength(item)}
+              className={`
+                inline-flex items-center
+                rounded-full
+                border
+                px-4 py-2
+                text-sm font-medium
+                cursor-pointer
+                transition-all duration-200 ease-in-out
+                active:scale-95
+
+                ${
+                  selectedLength.text === item.text
+                    ? "border-blue-600 bg-blue-600 text-white shadow-md"
+                    : "border-blue-200 bg-blue-50 text-blue-600 hover:-translate-y-0.5 hover:border-blue-400 hover:bg-blue-100 hover:shadow-sm"
+                }
+              `}
+            >
+              {item.text}
+            </span>
+          ))}
+        </div>
+        <br />
+        <button
+          type="submit"
+          className="
+    mt-6
+    w-full
+    flex items-center justify-center gap-2
+    rounded-xl
+    bg-linear-to-r from-[#4A7AFF] to-[#6C5CE7]
+    px-5 py-3
+    text-sm font-semibold text-white
+    shadow-md shadow-blue-500/20
+    transition-all duration-300 ease-out
+    hover:-translate-y-0.5
+    hover:shadow-lg hover:shadow-blue-500/30
+    active:translate-y-0
+    active:scale-[0.98]
+    disabled:cursor-not-allowed
+    disabled:opacity-60
+    cursor-pointer
+  "
+        >
+          <Edit className="w-5 h-5" />
+          Generate article
+        </button>
+      </form>
+
+      {/* Right Side */}
+      <div className="w-full max-w-lg p-6 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-150">
+        <div className="flex items-center gap-3">
+          <Edit className="w-5 h-5 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">Genrate Article</h1>
+        </div>
+
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Edit className="w-9 h-9" />
+            <p>Enter a topic and click “Generate article ” to get started</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default WriteAreticle
+export default WriteAreticle;
